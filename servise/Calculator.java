4141230b9cfdb58c67373b2687b9ec84ed8d42f0
Anatolii <novoselov39@gmail.com>
2023-10-26 22:55:26 +0500

    seminar 7 + log in file

--- servise/Calculator.java
+++ servise/Calculator.java
@@ -1,33 +1,49 @@
 package oop.seminar_7.example.OOPSeminar_7.servise;
 
+import java.io.FileInputStream;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
 import java.util.logging.Logger;
+
 
 import oop.seminar_7.example.OOPSeminar_7.data.Complex;
 
 public class Calculator {
+ 
 
-    private static final Logger LOGGER = Logger.getLogger(Calculator.class.getName());
+    static Logger LOGGER;
+    static {
+        try(FileInputStream ins = new FileInputStream("C:\\Users\\novos\\Documents\\Даша\\java\\oop\\seminar_7\\example\\OOPSeminar_7\\Loger\\Log.config")){
+            LogManager.getLogManager().readConfiguration(ins);
+            LOGGER = Logger.getLogger(Calculator.class.getName());
+        }catch (Exception ignore){
+            ignore.printStackTrace();
+        }
+    }
+    
+    
 
     public Complex add(Complex a, Complex b){
-        LOGGER.info("Операция сложения комплексных чисел");
+        LOGGER.log(Level.CONFIG,"Operation add");
         Complex result = a.add(b);
-        LOGGER.info("Результат: " + result.toString());
+        LOGGER.info("Result: " + result.toString());
         return result;
     }
 
 
     public Complex multiply(Complex a, Complex b){
-        LOGGER.info("Операция умножения комплексных чисел");
+        //LOGGER.log(Level.INFO,"Начало main, создаем лист с типизацией Integers");
+        LOGGER.info("Operation multiply");
         Complex result = a.multiply(b);
-        LOGGER.info("Результат: " + result.toString());
+        LOGGER.info("Result: " + result.toString());
         return result;
     }
 
 
-    public Complex divide(Complex a, Complex b){
-        LOGGER.info("Операция деления комплексных чисел");
+    public Complex divide(Complex a, Complex b){        
+        LOGGER.info("Operation divide");
         Complex result =  a.divide(b);
-        LOGGER.info("Результат: " + result.toString());
+        LOGGER.info("Result: " + result.toString());
         return result;
     }
     
